3fe4195dd92237cef1194f7e55e0fbdff05b645e
goncalvs98 <goncgbr@gmail.com>
2024-09-07 15:27:04 -0300

    retirando arquivos js

--- ex1_ta.js
+++
@@ -1,9 +0,0 @@
-function realizarCompra(produto, pagamento) {
-    return "Voc\u00EA comprou o produto ".concat(produto.nome, " por R$").concat(produto.preco, ", na categoria ").concat(produto.categoria, ", com pagamento via ").concat(pagamento, ".");
-}
-var produto1 = {
-    nome: "Notebook",
-    preco: 3500,
-    categoria: "Eletrônicos"
-};
-console.log(realizarCompra(produto1, 'pix'));
--- produto.js
+++
@@ -1,9 +0,0 @@
-function exibirInformacoes(livro) {
-    return "O livro \"".concat(livro.titulo, "\" foi escrito por ").concat(livro.autor, " e publicado em ").concat(livro.anoPublicacao, ".");
-}
-var livro1 = {
-    titulo: "O Senhor dos Anéis",
-    autor: "J.R.R. Tolkien",
-    anoPublicacao: 1954,
-};
-console.log(exibirInformacoes(livro1));
